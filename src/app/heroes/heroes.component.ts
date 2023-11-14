617b852a28b68239a827b0dbce08b3fce83d2fb8
cgarzoon <carlitosjuanca23@gmail.com>
2023-11-14 20:10:38 +0100

    Creación HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import {FormsModule} from '@angular/forms'
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
+  standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  imports: [CommonModule,
+            FormsModule,
+          HeroDetailComponent],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
