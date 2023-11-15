680fb9ba9b40147f9c6af34b4a40c04613a67c9a
cgarzoon <carlitosjuanca23@gmail.com>
2023-11-15 15:26:01 +0100

    Antes del App Routing Module

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import {FormsModule} from '@angular/forms'
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   standalone: true,
@@ -14,12 +15,31 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
             FormsModule,
           HeroDetailComponent],
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  
   selectedHero?: Hero;
 
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  //Gestiona el click sobre un heroe específico 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+
+
+  //Obtiene los heroes a través del servicio (HTTP y obtenemos un Observable<Hero[]>)
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+
 }
 
